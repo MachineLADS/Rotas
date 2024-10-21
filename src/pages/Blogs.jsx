5c8afb844976b8555ee0f4ed33949f65d913a932
LucasAmorim <lucas.amroim@iesb.edu.br>
2024-10-21 10:55:02 -0300

    Rotas

---
+++ src/pages/Blogs.jsx
@@ -0,0 +1,12 @@
+const Blogs = () => {
+  return <div> <h1>Blog Articles</h1>
+     <h2>Passatempos:</h2>
+     <ul>
+     <li>Passear</li>
+     <li>Jogar</li>
+     </ul>
+  </div>
+}
+
+
+export default Blogs
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+const Contact = () => {
+    return <div><h1><b>Contact Me</b></h1>
+    <ul>
+        <li>Nome: Anonimo</li>
+        <li>Email: Anomino@gamil.com</li>
+        <li>Numero: 99999-9999</li>
+    </ul>
+    </div>
+}
+  
+export default Contact;
